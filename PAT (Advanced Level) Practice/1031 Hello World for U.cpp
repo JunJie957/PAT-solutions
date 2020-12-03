42df5d7204c585c60ba5b940ded196b95a230694
Junjie957 <johnathan1874@163.com>
2020-12-03 15:59:59 +0800

    change answer of 1031

--- PAT (Advanced Level) Practice/1031 Hello World for U.cpp
+++ PAT (Advanced Level) Practice/1031 Hello World for U.cpp
@@ -4,17 +4,12 @@ using namespace std;
 int main() {
 	string str;
 	cin >> str;
-	int len = str.length(), side = 0, low = 0;
-	for (int i = len; i >= 3; --i) {
-		if ((len + 2 - i) % 2 == 0) {
-			side = (len + 2) / 3 - 1;
-			low = len - side * 2;
-			break;
-		}
-	}
+	int len = str.length(), side = (len + 2) / 3 - 1, low = len - side * 2;
 	for (int i = 0; i < side; ++i) {
 		printf("%c", str[i]);
-		for (int j = 1; j < low - 1; ++j) printf(" ");
+		for (int j = 1; j < low - 1; ++j) {
+			printf(" ");
+		}
 		printf("%c\n", str[len - i - 1]);
 	}
 	cout << str.substr(side, low);
